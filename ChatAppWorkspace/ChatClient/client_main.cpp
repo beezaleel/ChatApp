aa4f5a124f1c4c52b1d57d7270002536531c848b
Ademola Adedeji <ademola.adedeji@hotmail.com>
2022-10-17 13:55:17 -0400

    Added keyboard input for client

--- ChatAppWorkspace/ChatClient/client_main.cpp
+++ ChatAppWorkspace/ChatClient/client_main.cpp
@@ -1,6 +1,40 @@
 #include "Client.h"
+#include <conio.h>
+#include <string>
+
+std::string message = "";
+std::string name = "";
+
+void ProcessMessage() {}
+
+void ProcessKeyboardInput() {
+	if (_kbhit) {
+		int key = _getch();
+		switch (key)
+		{
+		case 13:
+			printf("Send message to server! \n");
+			message.clear();
+			break;
+		case 27:
+			printf("Exiting program . . . \n");
+			exit(0);
+		case 8:
+			if (!message.empty())
+				message.pop_back();
+			break;
+		default:
+			message += (char)key;
+			printf("%c", (char)key);
+			break;
+		}
+	}
+}
 
 int main(int argc, char* argv) {
+	while (true) {
+		ProcessKeyboardInput();
+	}
 	int success = 0;
 	Client client = Client();
 	client.Initialize();
